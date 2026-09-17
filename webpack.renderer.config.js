fd60ad2daa17edc21ceb8e53b5c59b4d5ceff700
Anthony Mittz <anthonymittz@gmail.com>
2026-09-17 19:44:09 +0400

    Added React

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -5,6 +5,17 @@ rules.push({
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
 
+rules.push({
+  test: /\.jsx?$/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      exclude: /node_modules/,
+      presets: ['@babel/preset-react']
+    }
+  }
+});
+
 module.exports = {
   devtool: 'source-map',
   module: {
