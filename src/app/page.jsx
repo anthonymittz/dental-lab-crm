0f08b3a85cb31e75b26ff46943d525f48fe45183
Anthony Mittz <anthonymittz@gmail.com>
2026-09-17 19:50:45 +0400

    Added Tailwind

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.body);
-root.render(<h2>React app!</h2>);
+root.render(<h2 className='font-bold'>React app!</h2>);
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,8 +1,14 @@
 const rules = require('./webpack.rules');
+const path = require('path');
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  include: path.resolve(__dirname, 'src'),
+  use: [
+    'style-loader',
+    'css-loader',
+    'postcss-loader'
+  ],
 });
 
 rules.push({
@@ -11,7 +17,8 @@ rules.push({
     loader: 'babel-loader',
     options: {
       exclude: /node_modules/,
-      presets: ['@babel/preset-react']
+      presets: ['@babel/preset-react'],
+      compact: false
     }
   }
 });
