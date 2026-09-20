f87fe7eadf9fe801ddbeac2c2528ff291579bc42
Anthony Mittz <anthonymittz@gmail.com>
2026-09-20 13:35:26 +0400

    Refactored IPC

--- src/ipc.js
+++ src/ipc.js
@@ -3,25 +3,43 @@
  * @property {(data: any) => Promise<string>} ping
  */
 
-/** @type {{[K in keyof IpcAPI as Uppercase<K>]: K}} */
+/** 
+ * IPC channels.
+ * @type {{[K in keyof IpcAPI as Uppercase<K>]: K}} 
+ */
 const IPC = {
   PING: 'ping'
-}
+};
 
-/** @type {IpcAPI} */
+/**
+ * IPC API to use in renderers.
+ * @type {IpcAPI} 
+ */
 const ipc = {
   ping: async data => await window.ipc.ping(data),
 };
 
-export function initIpc(ipcMain) {
+/**
+ * Map IPC actions to respective handlers (on the Main side).
+ * @param {import("electron").IpcMain} ipcMain 
+ * @param {{[K in keyof IpcAPI]?: IpcAPI[K]}} handlers 
+ */
+export function initIpc(ipcMain, handlers = {}) {
   ipcMain.handle(IPC.PING, (event, data) => 
     ({ message: "Pinging back", payload: data }));
 }
 
+/**
+ * Expose IPC actions for renderers (in Preload).
+ * @param {import("electron").ContextBridge} contextBridge 
+ * @param {import("electron").IpcRenderer} ipcRenderer 
+ */
 export function bridgeIpc(contextBridge, ipcRenderer) {
-  contextBridge.exposeInMainWorld('ipc', {
+  /** @type {IpcAPI} */
+  const api = {
     ping: async data => await ipcRenderer.invoke(IPC.PING, data),
-  });
+  };
+  contextBridge.exposeInMainWorld('ipc', api);
 }
 
 export default ipc;
