0b969eb59d4b57ce8a56004252181b3dff082519
Anthony Mittz <anthonymittz@gmail.com>
2026-09-23 11:54:06 +0400

    Added Button component

---
+++ src/components/elements/Button.jsx
@@ -0,0 +1,45 @@
+import { is, merge } from "@lib/merge";
+
+/**
+ * @param {{ 
+ *   className?: Partial<ReturnType<appearance>>
+ *   type?: "primary"|"secondary"|"outline"|"flat"
+ *   size?: "fit"|"sm"|"md"|"lg"
+ *   disabled?: boolean
+ *   children?: import("react").ReactNode
+ *   onClick?: () => void
+ * }} props
+ */
+function Button({
+  className,
+  type = 'primary', 
+  size = 'md',
+  disabled = false,
+  children = null,
+  onClick = () => {}
+}) {
+  const a = merge(appearance(disabled), className || {});
+  const style = is(a.container, a[type], a[size]);
+  return <button className={style} onClick={onClick}>{ children }</button>;
+}
+
+const appearance = disabled => ({
+  container: is("rounded-sm", disabled || 'cursor-pointer'),
+  primary: disabled 
+    ? "bg-accent-700 text-accent-600" 
+    : "bg-accent-500 text-test-900",
+  secondary: disabled 
+    ? "bg-test-300 text-test-400" 
+    : "bg-test-300 text-test-900 hover:bg-test-400",
+  outline: disabled 
+    ? "border border-test-300 text-test-300" 
+    : "text-test-900 border border-test-300 hover:border-test-400 hover:bg-test-300",
+  flat: "bg-transparent",
+  fit: "h-fit! w-fit!",
+  sm: "px-2 py-1",
+  md: "px-4 py-2",
+  lg: "px-6 py-4",
+});
+
+
+export default Button;
