3bb3373923d41f5a9716c70a3f1ef2bcb3f37a21
Anthony Mittz <anthonymittz@gmail.com>
2026-09-26 18:00:08 +0400

    Moved table data definition to JSON

--- src/pages/inventory/Materials.jsx
+++ src/pages/inventory/Materials.jsx
@@ -2,35 +2,9 @@ import CollapsibleList, { dataToItems } from "@/components/elements/CollapsibleL
 import Table from "@/components/elements/Table.jsx";
 import PaneLayout from "@/components/layouts/PaneLayout.jsx";
 import data from "@/content/inventory/materials.json";
+import tableData from "@/content/inventory/table.json";
 import { merge } from "@lib/merge";
 import { inventoryMaterials } from "@/content/descriptions.js";
-
-/** @type {TablePayload} */
-const table = {
-  columns: {
-    ids: ['id', 'name', 'quantity', 'location'],
-    visible: ['name', 'quantity', 'location'],
-    names: {
-      id: 'ID',
-      name: 'Name',
-      quantity: 'Quantity',
-      location: 'Location'
-    },
-    sizes: {
-      id: '0fr',
-      name: '2fr',
-      quantity: '1fr',
-      location: '1fr'
-    },
-    sortBy: "name",
-    ascending: true
-  },
-  data: [
-    ['1', 'Tape', 2, 'Shelf A'],
-    ['2', 'Glue', 1, 'Shelf B'],
-    ['3', 'Drilling bit', 4, 'Shelf C'],
-  ]
-}
 
 /**
  * @param {{ 
@@ -43,7 +17,7 @@ function Materials({ className = appearance }) {
   return (
     <PaneLayout flow="column" template="16rem 1fr">
       <CollapsibleList items={dataToItems(data)} className={{ container: 'p-2 bg-c-200'}} />
-      <Table initial={table} />
+      <Table initial={tableData} />
     </PaneLayout>
   );
 }
